4cb2d31868cd05068574f23816921aa676de3c6d
Tung Vu <gmanvn@gmail.com>
2017-02-24 23:13:36 +0700

    Pure jest solution
    also test if props are passed correctly

--- __test__/mapProps.test.js
+++ __test__/mapProps.test.js
@@ -1,21 +1,16 @@
-import { spy } from 'sinon';
 import { mount } from 'enzyme';
 import React from 'react';
 import mapProps from '../mapProps';
 
 describe('mapProps', () => {
   it('should be map props successfully', () => {
-    const Component = spy(() => null);
-    const EnhanceComponent = mapProps(props => ({
-      ...props,
-      foo: 'bar',
-    }))(Component);
+    const Component = jest.fn(() => null);
+    const mapFunction = jest.fn(() => ({ foo: 'bar' }));
+    const EnhanceComponent = mapProps(mapFunction)(Component);
 
-    mount(<EnhanceComponent dog='cat' />);
+    mount(<EnhanceComponent dog='cat' />, {});
 
-    const expected = { dog: 'cat', foo: 'bar' };
-    const actual = Component.firstCall.args[0];
-
-    expect(actual).toEqual(expected);
+    expect(mapFunction).toBeCalledWith({ dog: 'cat' })
+    expect(Component.mock.calls[0][0]).toEqual({"foo": "bar"});
   });
 });
