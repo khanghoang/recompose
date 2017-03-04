1df3abbbd6a045dabba1753b24c4ab750d99f2f0
Toan Nguyen <toan2406@gmail.com>
2017-03-04 15:51:12 +0700

    add test for compose function

---
+++ __test__/compose.test.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import compose from '../compose';
+
+describe('pure', () => {
+  it('should work properly', () => {
+    const func1 = jest.fn(x => x + 2);
+    const func2 = jest.fn(x => x * 3);
+    const func3 = jest.fn(x => x - 1);
+    const val = 10;
+    const result = compose(
+      func1,
+      func2,
+      func3
+    )(val);
+    expect(result).toEqual(func1(func2(func3(val))));
+  });
+});
